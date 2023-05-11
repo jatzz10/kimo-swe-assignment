509b5d95e3c1833e6f7c7769c768adbd485f6107
jatzz10 <jatinsharma2751997@gmail.com>
2023-05-12 03:47:15 +0530

    Add finishing touch

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,3 @@ RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 COPY . /app
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
-
-
-# FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
-# COPY requirements.txt /app/requirements.txt
-# RUN pip install -r /app/requirements.txt
-# COPY app /app/app
-# COPY tests /app/tests
-# COPY config.py /app/config.py
-# COPY main.py /app/main.py
-
-# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from pymongo import MongoClient
 
 
 def connect_mongodb():
-    MONGODB_USER = 'jsharmaishere' # os.environ.get('MONGODB_USER')
-    MONGODB_PASSWORD = 'KCXRcwocxBBgKsWR' # os.environ.get('MONGODB_PASSWORD')
-    MONGODB_HOST = 'cluster0.b4ad5xn.mongodb.net' # os.environ.get('MONGODB_HOST')
+    MONGODB_USER = os.environ.get('MONGODB_USER')
+    MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
+    MONGODB_HOST = os.environ.get('MONGODB_HOST')
 
     # Connect to MongoDB
     client = MongoClient(f'mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}')
